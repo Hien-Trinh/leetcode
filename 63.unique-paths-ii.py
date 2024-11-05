aa4af189b476aa72cfee359c15fa97bbc83bda88
Hien-Trinh <hientrinhminh32@gmail.com>
2024-11-05 14:17:03 -0600

    Add unique-paths-ii

---
+++ 63.unique-paths-ii.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=63 lang=python3
+#
+# [63] Unique Paths II
+#
+
+# @lc code=start
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        obstacleGrid[0][0] = 1
+        for i in range(1, len(obstacleGrid)):
+            if obstacleGrid[i][0] == 0:
+                obstacleGrid[i][0] = obstacleGrid[i - 1][0]
+            else:
+                obstacleGrid[i][0] = 0
+        for j in range(1, len(obstacleGrid[0])):
+            if obstacleGrid[0][j] == 0:
+                obstacleGrid[0][j] = obstacleGrid[0][j - 1]
+            else:
+                obstacleGrid[0][j] = 0
+
+        for i in range(1, len(obstacleGrid)):
+            for j in range(1, len(obstacleGrid[0])):
+                if obstacleGrid[i][j] != 1:
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + \
+                        obstacleGrid[i][j - 1]
+                else:
+                    obstacleGrid[i][j] = 0
+
+        return obstacleGrid[-1][-1]
+# @lc code=end
