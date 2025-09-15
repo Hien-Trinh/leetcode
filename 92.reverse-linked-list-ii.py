9670f955bedef9a02fe2155e8008df0397bb9857
Hien-Trinh <hientrinhminh32@gmail.com>
2025-09-15 17:48:17 -0500

    Add reverse linked list ii

---
+++ 92.reverse-linked-list-ii.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=92 lang=python3
+#
+# [92] Reverse Linked List II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if left == right or not head:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        for _ in range(left - 1):
+            prev = prev.next
+
+        cur = prev.next
+        for _ in range(right - left):
+            temp = cur.next
+            cur.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+
+        return dummy.next
+# @lc code=end
+
