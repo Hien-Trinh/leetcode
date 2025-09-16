aed0f41782decae1b5375c894e9ad36da4f758d0
Hien-Trinh <hientrinhminh32@gmail.com>
2025-09-15 21:19:31 -0500

    Add maximal square

---
+++ 221.maximal-square.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=221 lang=python3
+#
+# [221] Maximal Square
+#
+
+# @lc code=start
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        if m == n == 1:
+            return int(matrix[0][0])
+
+        dp = [[0] * n for _ in range(m)]
+
+        max_width = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+
+                    max_width = max(max_width, dp[i][j])
+
+        return max_width * max_width
+# @lc code=end
+
