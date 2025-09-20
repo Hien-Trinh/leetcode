910c4cc5647d2c9984821b58537e0595a03af106
Hien-Trinh <hientrinhminh32@gmail.com>
2025-09-20 08:37:31 -0500

    Add kids-with-the-greatest-number-of-candies

---
+++ 1431.kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=1431 lang=python3
+#
+# [1431] Kids With the Greatest Number of Candies
+#
+
+# @lc code=start
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        threshold = max(candies)
+        return [candy + extraCandies >= threshold for candy in candies]
+# @lc code=end
+
