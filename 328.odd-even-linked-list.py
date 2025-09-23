226149a950cfe3d08cce0cac523d23dd8bc0cadd
Hien-Trinh <hientrinhminh32@gmail.com>
2025-09-23 08:49:06 -0500

    Add odd-even-linked-list

---
+++ 328.odd-even-linked-list.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=328 lang=python3
+#
+# [328] Odd Even Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        even, odd = head.next, head
+        even_head = even
+
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+
+        odd.next = even_head
+
+        return head
+# @lc code=end
+
