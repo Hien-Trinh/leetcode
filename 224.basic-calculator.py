0920e9151d99d9ca3235da3861217f59962088a9
Hien-Trinh <hientrinhminh32@gmail.com>
2025-09-24 20:15:19 -0500

    Add basic-calculator

---
+++ 224.basic-calculator.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=224 lang=python3
+#
+# [224] Basic Calculator
+#
+
+# @lc code=start
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+
+        num, sign, res = 0, 1, 0
+        for c in s:
+            if c.isdigit():
+                num = num * 10 + int(c)
+            elif c in "+-":
+                res += num * sign
+                sign = 1 if c == "+" else -1
+                num = 0
+            elif c == "(":
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif c == ")":
+                res += sign * num
+                res *= stack.pop()
+                res += stack.pop()
+                num = 0
+
+        return res + sign * num
+# @lc code=end
+
